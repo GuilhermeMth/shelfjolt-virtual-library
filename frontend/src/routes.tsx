38437923c871f6396a7ce6ed2c44cc86e4ff3fdb
Marcelo Henrique Almeida da Silva <marcelo.hasilva06@gmail.com>
2026-02-18 00:12:19 -0300

    feat: Adiciona Tela de Cadastro

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/index.tsx";
+import Cadastro from "./pages/cadastro.tsx";
 
 export function MainRoutes() {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
 			<Route path="*" element={<Home />} />
+			<Route path="/cadastro" element={<Cadastro />} />
 		</Routes>
 	);
 }
