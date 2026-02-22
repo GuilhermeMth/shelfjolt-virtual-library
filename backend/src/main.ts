e9633f6f997110335767f867bcada2a7062876db
GuilhermeMth <guilhermemfranca06@gmail.com>
2026-02-22 12:37:50 -0300

    refactor: update dependencies and improve authentication flow
    - Updated ESLint and TypeScript versions in package.json.
    - Created a new Storage utility for managing access tokens in localStorage.
    - Refactored formCadastro component to utilize the new Storage utility for token management and improved error handling.
    - Refactored formLogin component to use the new API service for login functionality and token management.
    - Added a new API service for handling API requests with authorization.
    - Implemented a new auth service for registration and login processes, including password confirmation checks.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,13 +3,18 @@ import express from "express";
 import cors from "cors";
 import router from "./routes";
 
+const origin = process.env.FRONTEND_URL;
+const port = Number(process.env.API_PORT);
+
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: `${origin}`,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use("/api", router);
-
-const port = Number(process.env.API_PORT) || 3000;
-
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
